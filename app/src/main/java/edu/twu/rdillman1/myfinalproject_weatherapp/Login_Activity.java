488d6e3e6acccd015fe6eba2da7deac96ddbf7a8
Gentry Dillman <rdillman1@twu.edu>
2021-04-10 05:51:08 -0500

    clearup push

---
+++ app/src/main/java/edu/twu/rdillman1/myfinalproject_weatherapp/Login_Activity.java
@@ -0,0 +1,14 @@
+package edu.twu.rdillman1.myfinalproject_weatherapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Login_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_);
+    }
+}
---
+++ app/src/main/java/edu/twu/rdillman1/myfinalproject_weatherapp/Spash_Activity.java
@@ -0,0 +1,14 @@
+package edu.twu.rdillman1.myfinalproject_weatherapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Spash_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spash_);
+    }
+}
