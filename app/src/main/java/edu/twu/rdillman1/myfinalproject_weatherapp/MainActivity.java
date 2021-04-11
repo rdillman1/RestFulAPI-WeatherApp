cbe941048e9f5151b4417480be540f047916a935
Gentry Dillman <rdillman1@twu.edu>
2021-04-11 14:30:22 -0500

    Version 1.3 Google location services working and am able to get current location on startup!

--- app/src/main/java/edu/twu/rdillman1/myfinalproject_weatherapp/MainActivity.java
+++ app/src/main/java/edu/twu/rdillman1/myfinalproject_weatherapp/MainActivity.java
@@ -1,8 +1,20 @@
 package edu.twu.rdillman1.myfinalproject_weatherapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -13,22 +25,49 @@ import android.widget.Toast;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+import static android.location.LocationManager.GPS_PROVIDER;
+
+public class MainActivity extends AppCompatActivity implements LocationListener  {
     private FirebaseAuth mAuth;
-public TextView tv_name;
+
+    // Add code here to register the listener with the Location Manager to receive location updates
+    LocationManager locationManager;
+   // private static final int TAKE_PHOTO_PERMISSION = 1;
+    LocationListener locationListener;
+    Double currentLat;
+    Double currentLon;
+    TextView tv_name;
+    TextView tv_location;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         mAuth = FirebaseAuth.getInstance();
         FirebaseUser user = mAuth.getCurrentUser();
         tv_name = findViewById(R.id.welcomeID);
+        tv_location = findViewById(R.id.tv_locationName);
         tv_name.setText(user.getDisplayName());
-        Spinner s =(Spinner) findViewById(R.id.settings);
+        Spinner s = (Spinner) findViewById(R.id.settings);
+
+//Check permissions
+        if (Build.VERSION.SDK_INT >= 27 &&
+                ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+        }
+// Get location
+    getLocation();
+//setting dropdown menu
         s.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-              String selected = parent.getItemAtPosition(position).toString();
+                String selected = parent.getItemAtPosition(position).toString();
                 switch (selected){
                     case "Settings":
                         //do nothing
@@ -46,9 +85,6 @@ public TextView tv_name;
                         break;
                     case "Option 2":
                         Toast.makeText(getApplicationContext(),"I am testing option 2",Toast.LENGTH_SHORT).show();
-                    default:
-                       //do nothing
-                        break;
                 }
             }
 
@@ -61,5 +97,34 @@ public TextView tv_name;
 
 
 
+
+
+
+    }
+
+    @SuppressLint("MissingPermission")
+    private void getLocation() {
+        try {
+            locationManager = (LocationManager) getApplicationContext().getSystemService(LOCATION_SERVICE);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,5000,50,this);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        currentLon = location.getLongitude();
+        currentLat = location.getLatitude();
+        Toast.makeText(getApplicationContext()," "+ currentLat + currentLon,Toast.LENGTH_LONG).show();
+   try{
+       Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+       List<Address> addresses = geocoder.getFromLocation(currentLat, currentLon, 1);
+       String address = addresses.get(0).getLocality();
+       tv_location.setText(address);
+       Toast.makeText(getApplicationContext()," "+ address,Toast.LENGTH_LONG).show();
+   }catch (Exception e){
+       e.printStackTrace();
+   }
     }
 }
